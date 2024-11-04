f8da1ec029f87d8b1ea6c4255da1f1269255d8d9
Abin Binu <abinbinu1827@gmail.com>
2024-11-04 15:22:14 +0530

    error in building second time

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',  // Replace with your backend's port
+        target: 'http://localhost:3001', 
         changeOrigin: true,
-        secure: false, // Set to false if you're using HTTP
+        secure: false,
       },
     },
   },
