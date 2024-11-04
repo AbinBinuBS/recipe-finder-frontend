a11f3ae6da9d495d486b7f1e08583b69c1c7e46d
Abin Binu <abinbinu1827@gmail.com>
2024-11-04 16:36:21 +0530

    changing url in google

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RegisterBody = () => {
 
     const handleGoogleLogin = () =>{
-		window.location.href = "https://recipe-finder-backend-idhn.onrender.com";
+		window.location.href = "https://recipe-finder-backend-idhn.onrender.com/auth";
     }
 
 
