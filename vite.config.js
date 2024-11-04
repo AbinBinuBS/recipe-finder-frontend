835e0e13d949ca96d7a1ba2095dc227de8562343
Abin Binu <abinbinu1827@gmail.com>
2024-11-04 15:24:35 +0530

    error in building third time

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['lodash']
+    }
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', 
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
